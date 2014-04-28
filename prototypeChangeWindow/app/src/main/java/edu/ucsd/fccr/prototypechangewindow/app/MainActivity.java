492f381fdfdb869e51e52ba989cf3a52f326ae2b
Colin Togashi <ctogashi@ucsd.edu>
2014-04-27 20:46:40 -0700

    Passing Variables Through Activities

---
+++ prototypeChangeWindow/app/src/main/java/edu/ucsd/fccr/prototypechangewindow/app/MainActivity.java
@@ -0,0 +1,137 @@
+package edu.ucsd.fccr.prototypechangewindow.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    // initiate ListView
+    ListView listview;
+
+    public String[] nameArray = {"x1","x2","x3","x4"};
+    public double[] valueArray = {1,2,3,4};
+    public double[] minArray = {1,1,1,1};
+    public double[] maxArray = {5,5,5,5};
+    public ArrayList<String> valueStringArray = new ArrayList<String>();
+    public String[] valueSArray = {"1","2","3","4"};
+
+    private final int REQUEST_CODE = 1;
+    private int index;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.listView);
+
+        updateList();
+
+    }
+
+    private ArrayList<Map<String, String>> buildData() {
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        valueStringArray = new ArrayList<String>();
+
+        for(int i=0; i<nameArray.length; i++){
+
+            valueStringArray.add("Value = " + Double.toString(valueArray[i]));
+            list.add(putData(nameArray[i], valueStringArray.get(i)));
+            //list.add(putData(nameArray[i], valueSArray[i]));
+
+        }
+        return list;
+    }
+
+    private HashMap<String, String> putData(String name, String purpose) {
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("name", name);
+        item.put("purpose", purpose);
+        return item;
+    }
+
+    public void updateList(){
+
+        ArrayList<Map<String, String>> list = buildData();
+        String[] from = { "name", "purpose" };
+        int[] to = { android.R.id.text1, android.R.id.text2 };
+
+        SimpleAdapter adapter = new SimpleAdapter(this, list,
+                android.R.layout.simple_list_item_2, from, to);
+
+        listview.setAdapter(adapter);
+
+        listview.setTextFilterEnabled(true);
+
+        listview.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+                Intent data = new Intent(MainActivity.this, changeWindow.class);
+                data.putExtra("index", position);
+                data.putExtra("name", nameArray[position]);
+                data.putExtra("value", valueArray[position]);
+                data.putExtra("min", minArray[position]);
+                data.putExtra("max", maxArray[position]);
+                startActivityForResult(data, REQUEST_CODE);
+
+            }
+
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+
+            if(resultCode == RESULT_OK){
+                index = data.getIntExtra("index", 0);
+                valueArray[index] = data.getDoubleExtra("value", 100);
+                valueSArray[index] = Double.toString(valueArray[index]);
+            }
+            if(resultCode == RESULT_CANCELED){
+                Toast fail = Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT);
+                fail.show();
+            }
+        }
+
+        updateList();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ prototypeChangeWindow/app/src/main/java/edu/ucsd/fccr/prototypechangewindow/app/changeWindow.java
@@ -0,0 +1,152 @@
+package edu.ucsd.fccr.prototypechangewindow.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by CTogashi on 4/27/14.
+ */
+public class changeWindow extends Activity {
+
+    TextView variableNameTextView;
+    TextView variableMaxTextView;
+    TextView variableMinTextView;
+    EditText variableValueEditText;
+    Button okayButton;
+    Button cancelButton;
+
+    private int index;
+    private String name;
+    private double value;
+    private double newValue;
+    private double min;
+    private double max;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_window);
+
+        Intent fromMain = getIntent();
+        index = fromMain.getIntExtra("index", 0);
+        name = fromMain.getStringExtra("name");
+        value = fromMain.getDoubleExtra("value", 0);
+        min = fromMain.getDoubleExtra("min", 0);
+        max = fromMain.getDoubleExtra("max", 5);
+        newValue = value;
+
+        variableNameTextView = (TextView) findViewById(R.id.variableNameTextView);
+        variableMaxTextView = (TextView) findViewById(R.id.variableMaxTextView);
+        variableMinTextView = (TextView) findViewById(R.id.variableMinTextView);
+        variableValueEditText = (EditText) findViewById(R.id.variableValueEditText);
+        okayButton = (Button) findViewById(R.id.okayButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        variableNameTextView.setText(name);
+        variableMaxTextView.setText("Max Value : " + Double.toString(max));
+        variableMinTextView.setText("Min Value : " + Double.toString(min));
+        variableValueEditText.setText(Double.toString(newValue));
+
+        variableValueEditText.addTextChangedListener(variableValueEditTextListener);
+        okayButton.setOnClickListener(okayButtonOnClickListener);
+        cancelButton.setOnClickListener(cancelButtonOnClickListener);
+
+    }
+
+    private TextWatcher variableValueEditTextListener = new TextWatcher(){
+
+        @Override
+        public void afterTextChanged(Editable arg0) {
+
+            if(Double.parseDouble(arg0.toString()) > max){
+
+                Toast maxError = Toast.makeText(getApplicationContext(),"Too High",Toast.LENGTH_SHORT);
+                maxError.show();
+                variableValueEditText.setText(Double.toString(value));
+
+            } else if(Double.parseDouble(arg0.toString()) < min){
+
+                Toast minError = Toast.makeText(getApplicationContext(),"Too Low",Toast.LENGTH_SHORT);
+                minError.show();
+                variableValueEditText.setText(Double.toString(value));
+
+            } else {
+
+                newValue = Double.parseDouble(arg0.toString());
+
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                      int arg3) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                                  int arg3) {
+
+            try{
+
+                if(Double.parseDouble(arg0.toString()) > max){
+
+                    Toast maxError = Toast.makeText(getApplicationContext(),"Too High",Toast.LENGTH_SHORT);
+                    maxError.show();
+
+                } else if(Double.parseDouble(arg0.toString()) < min){
+
+                    Toast minError = Toast.makeText(getApplicationContext(),"Too Low",Toast.LENGTH_SHORT);
+                    minError.show();
+
+                } else {
+
+                    newValue = Double.parseDouble(arg0.toString());
+
+                }
+            }
+            catch(NumberFormatException e) {
+
+                Toast notANumber = Toast.makeText(getApplicationContext(),"Not a Number",Toast.LENGTH_SHORT);
+                notANumber.show();
+
+            }
+
+        }
+
+    };
+
+    private OnClickListener okayButtonOnClickListener = new OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("index", index);
+            returnIntent.putExtra("value", newValue);
+            setResult(RESULT_OK, returnIntent);
+            finish();
+        }
+
+    };
+
+    private OnClickListener cancelButtonOnClickListener = new OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            finish();
+
+        }
+
+    };
+
+}
